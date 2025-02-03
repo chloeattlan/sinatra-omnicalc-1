e359337c6679e797fd68377c8e3f7549d8c2dbac
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-03 07:54:50 +0000

    added square root and square

--- app.rb
+++ app.rb
@@ -6,13 +6,25 @@ get("/") do
 end
 
 get("/square/new") do
-  @num = params["user_num"]&.to_i
+  @num = params["users_number"]&.to_i
   erb :square
 end
 
 get("/square/results") do
-  @num = params["user_num"]&.to_i
-  @the_num = params.fetch("user_num").to_f
+  @num = params["users_number"]&.to_i
+  @the_num = params.fetch("users_number").to_f
   @the_result = @the_num**2
   erb :square_results
+end
+
+get("/square_root/new") do
+  @num = params["users_number"]&.to_i
+  erb :square_root
+end
+
+get("/square_root/results") do
+  @num = params["users_number"]&.to_i
+  @the_num = params.fetch("users_number").to_f
+  @the_result = Math.sqrt(@the_num)
+  erb :square_root_results
 end
